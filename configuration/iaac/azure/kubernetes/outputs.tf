315166e3e67e849804ec763e312cc0e799835335
Rivson J. Moraes <51989091+rivson@users.noreply.github.com>
2022-03-10 17:48:28 +0530

    adding sensitive   = true to all outputs

--- configuration/iaac/azure/kubernetes/outputs.tf
+++ configuration/iaac/azure/kubernetes/outputs.tf
@@ -1,17 +1,21 @@
 output "client_key" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_key
+  sensitive   = true
 }
 
 output "client_certificate" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_certificate
+  sensitive   = true
 }
 
 output "cluster_ca_certificate" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.cluster_ca_certificate
+  sensitive   = true
 }
 
 output "cluster_username" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.username
+  sensitive   = true
 }
 
 output "cluster_password" {
@@ -21,8 +25,10 @@ output "cluster_password" {
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config_raw
+  sensitive   = true
 }
 
 output "host" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.host
+  sensitive   = true
 }
